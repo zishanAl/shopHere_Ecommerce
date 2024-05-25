046a0738d5561e227325f00aab0525e7495d512d
Zishan <97729148+zishanAl@users.noreply.github.com>
2024-05-25 16:41:36 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,6 @@ import { useSelector } from "react-redux";
 
 const App = () => {
   const user = useSelector((state) => state.user.currentUser);
-  console.log(user);
   return (
     <Router>
       <Switch>
