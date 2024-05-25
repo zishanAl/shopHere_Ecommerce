9e99216d573fd79e4ff4c1e9f993cb90e833ceb4
Zishan <97729148+zishanAl@users.noreply.github.com>
2024-05-25 16:29:14 +0530

    Update Success.jsx

--- client/src/pages/Success.jsx
+++ client/src/pages/Success.jsx
@@ -8,7 +8,6 @@ import { userRequest } from "../requestMethods";
 
 const Success = () => {
   const location = useLocation();
-  //in Cart.jsx I sent data and cart. Please check that page for the changes.(in video it's only data)
   const data = location.state.stripeData;
   const cart = location.state.cart;
   const currentUser = useSelector((state) => state.user.currentUser);
@@ -58,5 +57,4 @@ const Success = () => {
     </div>
   );
 };
-// this page is not working because of stripe token
 export default Success;
